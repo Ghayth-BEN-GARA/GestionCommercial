a517b6f620d42cd9cc34eb3e05a471ebd81d58fe
Ghayth BenGara <ghayth.bengara.26@gmail.com>
2022-04-17 13:01:46 +0200

    Créer la page login, le layout head et le controller user

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,8 @@
+<meta charset = "utf-8">
+<meta name = "viewport" content = "width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Abbour Stock Depot</title>
+<link rel = "stylesheet" href = "{{asset('vendors/feather/feather.css')}}">
+<link rel = "stylesheet" href = "{{asset('vendors/ti-icons/css/themify-icons.css')}}">
+<link rel = "stylesheet" href = "{{asset('vendors/css/vendor.bundle.base.css')}}">
+<link rel = "stylesheet" href = "{{asset('css/vertical-layout-light/style.css')}}">
+<link rel = "shortcut icon" href = "{{asset('images/logo/favicon.png')}}">
