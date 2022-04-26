522b5561b14f8e38a1af4289f5080b085562e826
Ghayth BenGara <ghayth.bengara.26@gmail.com>
2022-04-26 13:25:07 +0200

    Créer la migration, le model et le controlleur pour le stock

---
+++ app/Models/Stock.php
@@ -0,0 +1,60 @@
+<?php
+    namespace App\Models;
+    use Illuminate\Database\Eloquent\Model;
+
+    class Stock extends Model{
+        protected $table = 'stocks';
+        protected $primaryKey = 'id';
+        public $timestamps = false;
+        public $incrementing = false;
+
+        /**
+         * The attributes that are mass assignable.
+         *
+         * @var array<int, string>
+         */
+        protected $fillable = [
+            'id',
+            'qteStock',
+            'qteTotale',
+            'prix',
+            'reference'
+        ];
+
+        public function getIdAttribute(){
+            return $this->attributes['id'];
+        }
+
+        public function getQteStockAttribute(){
+            return $this->attributes['qteStock'];
+        }
+
+        public function setQteStockAttribute($value){
+            $this->attributes['qteStock'] = $value;
+        }
+
+        public function getQteTotaleAttribute(){
+            return $this->attributes['qteTotale'];
+        }
+
+        public function setQteTotaleAttribute($value){
+            $this->attributes['qteTotale'] = $value;
+        }
+
+        public function getPrixAttribute(){
+            return $this->attributes['prix'];
+        }
+
+        public function setPrixAttribute($value){
+            $this->attributes['prix'] = $value;
+        }
+
+        public function getReferenceAttribute(){
+            return $this->attributes['reference'];
+        }
+
+        public function setReferenceAttribute($value){
+            $this->attributes['reference'] = $value;
+        }
+    }
+?>
